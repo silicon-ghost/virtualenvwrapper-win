6e288132026fee93e12be2a1b6194640646ce8f2
Matthew Zaleski <silicon_ghost33@zaleski.net>
2013-11-20 19:04:09 -0500

    Rewrote setup.py to be closer to Jeff Knupp style.

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _setuptools_install
+import io
+import codecs
+import os
+import sys
+import shutil
 
-from distribute_setup import use_setuptools
-use_setuptools()
 
-PROJECT = 'virtualenvwrapper-wintcc'
-AUTHOR = 'Matthew Zaleski'
-EMAIL = 'silicon_ghost25@zaleski.net'
-DESCRIPTION = ("Port of David Marble's virtualenvwrapper-win "
-               "Windows batch scripts to JPSoft's Take Command/TCC (tested with 12.x)") 
-VERSION = '0.9'
-PROJECT_URL = 'https://github.com/silicon_ghost/%s/' % (PROJECT)
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.rst', 'CHANGES.rst')
+
+__version__ = '0.9.1a1'
 scripts_loc = 'scripts/'
 scripts = [
     'add2virtualenv.btm',
@@ -27,20 +40,7 @@ scripts = [
     'workon.btm',
 ]
 
-import os
 import sys
-import shutil
-import codecs
-from setuptools import setup
-from setuptools.command.install import install as _setuptools_install
-
-long_description = ''
-try:
-    long_description = open('README.rst', 'rt').read()
-except IOError:
-    pass
-
-PYTHONHOME = sys.exec_prefix
 
 class install(_setuptools_install):
     def run(self):
@@ -57,56 +57,42 @@ class install(_setuptools_install):
                 for script in scripts:
                     if fname.endswith(script):
                         newname = fname.replace('Scripts\\','')
-                        # try:
-                        #     os.remove(dst)
-                        # except:
-                        #     pass
                         shutil.move(fname, newname)
                         fname = newname
                 newlist.append(fname)
             with codecs.open(self.record, 'w', 'utf-8') as f:
                 f.write('\n'.join(newlist))
 
+
 setup(
     cmdclass={'install': install},
-    name=PROJECT,
-    version=VERSION,
-
-    description=DESCRIPTION,
+    name='virtualenvwrapper-wintcc',
+    version=__version__,
+    url='https://github.com/silicon-ghost/virtualenvwrapper-wintcc',
+    license='MIT License',
+    author='Matthew Zaleski',
+    install_requires=['virtualenv>=1.9.1'],
+    author_email='silicon_ghost25@zaleski.net',
+    description="Port of David Marble's virtualenvwrapper-win (and Doug Hellman's virtualenvwrapper)"
+                "Windows batch scripts to JPSoft's Take Command/TCC",
     long_description=long_description,
-
-    author=AUTHOR,
-    author_email=EMAIL,
-    url=PROJECT_URL,
-    license="MIT License",
     keywords="setuptools deployment installation distutils virtualenv virtualenvwrapper virtualenvwrapper-win",
-
+    #include_package_data=True,
     platforms=['WIN32', 'WIN64',],
-
-    classifiers=[
+    scripts=[scripts_loc + script for script in scripts],
+    classifiers = [
         'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console', 
         'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.4',
-        #'Programming Language :: Python :: 2.5',
-        #'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.0',
-        #'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Intended Audience :: Developers',
-        'Environment :: Console', ],
-
-    scripts=[scripts_loc + script for script in scripts],
-
-    install_requires=['virtualenv>=1.9.1',],
-    
-    # extras
-    # pywin==0.2
-
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Build Tools',
+        ],
     zip_safe=False,
 )
